985cc37e7761c595ae8f8d5bb4087c6bc8490c58
VMahluza <mahluzavictor@gmail.com>
2024-05-07 09:18:15 +0200

    Initial Commit

---
+++ src/main/java/com/mycompany/gitpractice01/GitPractice01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitpractice01;
+
+/**
+ *
+ * @author mahlu
+ */
+public class GitPractice01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
